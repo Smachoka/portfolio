0461fa20f174751a6e0bb8e6c67b4bb1325429b4
Smachoka <silvanusmachoka@gmail.com>
2025-06-12 13:08:52 +0300

    Initial committ

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/portfolio/", // 👈 required for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
